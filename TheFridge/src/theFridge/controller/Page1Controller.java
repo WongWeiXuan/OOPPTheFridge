2c077544373340a4f049a6355d2d090863354f23
LimXuanZheng <limxuanzheng888@gmail.com>
2016-12-08 22:08:56 +0800

    Added my controller and scenebuilder

---
+++ TheFridge/src/theFridge/controller/Page1Controller.java
@@ -0,0 +1,125 @@
+package theFridge.controller;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.file.FileSystems;
+import java.util.Scanner;
+import java.io.File;
+
+import theFridge.RefrigeratorApp;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
+public class Page1Controller {
+
+    @FXML
+    private TableColumn<?, ?> table1;
+    @FXML
+    private TableColumn<?, ?> table2;
+    @FXML
+    private Button btnGenerateMultiple;
+    @FXML
+    private AnchorPane Anchor;
+    @FXML
+    private VBox naviPreview;
+    @FXML
+    private VBox navi;
+    @FXML
+    private VBox homeScene;
+    @FXML
+    private VBox recipeScene;
+    @FXML
+    private VBox compostScene;
+    @FXML
+    private VBox foodScene;
+    @FXML
+    private VBox quizScene;
+
+    @FXML
+    private VBox prizeScene;
+    
+
+	@FXML
+	public void showNavigation(MouseEvent event){
+		Timeline timeline = new Timeline();
+		KeyValue naviXValue = new KeyValue(navi.layoutXProperty(), 0);
+		KeyValue naviPreviewOpacity = new KeyValue(naviPreview.opacityProperty(), 0);
+		
+		KeyFrame keyFrame = new KeyFrame(Duration.millis(300), naviXValue, naviPreviewOpacity);
+		
+		timeline.getKeyFrames().addAll(keyFrame);
+		timeline.play();
+	}
+	
+	@FXML
+	public void hideNavigation(MouseEvent event){
+		Timeline timeline = new Timeline();
+		KeyValue naviXValue = new KeyValue(navi.layoutXProperty(), -150);
+		KeyValue naviPreviewOpacity = new KeyValue(naviPreview.opacityProperty(), 1);
+		
+		KeyFrame keyFrame = new KeyFrame(Duration.millis(300), naviXValue, naviPreviewOpacity);
+		
+		timeline.getKeyFrames().addAll(keyFrame);
+		timeline.play();
+	}
+	
+	@FXML
+	public void changeScene(MouseEvent event) throws IOException {
+		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		Parent root = (Parent)FXMLLoader.load(getClass().getResource("/FC/view/Page1.fxml"));
+		
+		
+		if(event.getSource().equals(homeScene)){
+			root = FXMLLoader.load(getClass().getResource(""));
+		}
+		else if(event.getSource().equals(recipeScene)){
+			root = FXMLLoader.load(getClass().getResource(""));
+		}
+		else if(event.getSource().equals(compostScene)){
+			root = FXMLLoader.load(getClass().getResource(""));
+		}
+		else if(event.getSource().equals(foodScene)){
+			root = FXMLLoader.load(getClass().getResource("FC/view/ShoppingList.fxml"));
+		}
+		else if(event.getSource().equals(quizScene)){
+			root = FXMLLoader.load(getClass().getResource(""));
+		}
+		else if(event.getSource().equals(prizeScene)){
+			root = FXMLLoader.load(getClass().getResource(""));
+		}
+
+ 		stage.setScene(new Scene(root));
+ 	    stage.show();
+	}
+	
+	@FXML
+	public void generateTwo(ActionEvent event)throws IOException {
+			Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+			Parent root = (Parent)FXMLLoader.load(getClass().getResource("/FC/view/Page1.fxml"));
+			root = FXMLLoader.load(getClass().getResource("/FC/view/Page2.fxml"));
+			stage.setScene(new Scene(root));
+	 	    stage.show();
+	}
+	
+	
+	@FXML
+	public void showMultipleFood(ActionEvent event){
+		
+	}
+}
