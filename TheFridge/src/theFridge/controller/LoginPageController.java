6c9aca6537334b06996fceb94aa550891dfc283f
Axomized <dr.que9@gmail.com>
2016-12-10 23:55:15 +0800

    Wei Xuan, is this how you generate controllers?

---
+++ TheFridge/src/theFridge/controller/LoginPageController.java
@@ -0,0 +1,29 @@
+package theFridge.controller;
+
+import java.io.IOException;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LoginPageController {
+	@FXML
+	private JFXButton loginBtn;
+	@FXML
+	private JFXButton signupBtn;
+	
+	@FXML
+	void goToHomePage(ActionEvent event) throws IOException {
+		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		Parent root = (Parent)FXMLLoader.load(getClass().getResource("/theFridge/view/HomePage.fxml"));
+		
+		stage.setScene(new Scene(root));
+ 	    stage.show();
+	}
+}
---
+++ TheFridge/src/theFridge/controller/SignupPageController.java
@@ -0,0 +1,23 @@
+package theFridge.controller;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class SignupPageController {
+	@FXML
+	private JFXButton loginBtn;
+	@FXML
+	private JFXButton createAccount;
+	
+	@FXML
+	void goToLoginPage(ActionEvent event) {
+		
+	}
+	
+	@FXML
+	void createAccount(ActionEvent event) {
+		
+	}
+}
