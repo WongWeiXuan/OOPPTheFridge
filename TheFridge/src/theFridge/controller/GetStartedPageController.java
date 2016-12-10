67ea3bbe4496c37055e60e2dbc8d803cc06ae7f1
WongWeiXuan <wong_weixuan@hotmail.com>
2016-12-10 23:23:14 +0800

    Homepage official

---
+++ TheFridge/src/theFridge/controller/GetStartedPageController.java
@@ -0,0 +1,73 @@
+package theFridge.controller;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GetStartedPageController {
+	@FXML
+	private VBox Profile;
+	@FXML
+	private VBox Recipes;
+	@FXML
+	private VBox Compost;
+	@FXML
+	private VBox Calculator;
+	@FXML
+	private VBox Quiz;
+	@FXML
+	private VBox Redeem;
+	@FXML
+	private VBox Shopping;
+	@FXML
+	private VBox Leftover;
+	@FXML
+	private VBox Logout;
+
+	// Event Listener on VBox[#Profile].onMouseClicked
+	@FXML
+	public void changeScene(MouseEvent event) throws IOException {
+		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		Parent root = null; //(Parent)FXMLLoader.load(getClass().getResource("/theFridge/view/HomePage.fxml"));
+		
+		
+		if(event.getSource().equals(Profile)){
+			root = FXMLLoader.load(getClass().getResource("/theFridge/view/Profile.fxml"));
+		}
+		else if(event.getSource().equals(Recipes)){
+			root = FXMLLoader.load(getClass().getResource("/theFridge/view/RecipePage.fxml"));
+		}
+		else if(event.getSource().equals(Compost)){
+			root = FXMLLoader.load(getClass().getResource("/theFridge/view/Page1.fxml"));
+		}
+		else if(event.getSource().equals(Calculator)){
+			root = FXMLLoader.load(getClass().getResource("/theFridge/view/ShoppingListPage.fxml"));
+		}
+		else if(event.getSource().equals(Quiz)){
+			root = FXMLLoader.load(getClass().getResource("/theFridge/view/QuizPage.fxml"));
+		}
+		else if(event.getSource().equals(Redeem)){
+			root = FXMLLoader.load(getClass().getResource("/theFridge/view/RedeemVoucherPage.fxml"));
+		}
+		else if(event.getSource().equals(Shopping)){
+			root = FXMLLoader.load(getClass().getResource("/theFridge/view/ShoppingListPage.fxml"));
+		}
+		else if(event.getSource().equals(Leftover)){
+			root = FXMLLoader.load(getClass().getResource("/theFridge/view/.fxml"));
+		}
+		else if(event.getSource().equals(Logout)){
+			root = FXMLLoader.load(getClass().getResource("/theFridge/view/.fxml"));
+		}
+		
+ 		stage.setScene(new Scene(root));
+ 	    stage.show();
+	}
+}
--- TheFridge/src/theFridge/controller/HomePageController.java
+++ TheFridge/src/theFridge/controller/HomePageController.java
@@ -5,105 +5,35 @@ import java.util.Random;
 
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import com.jfoenix.controls.JFXButton;
 
 public class HomePageController {
-	@FXML
-	private AnchorPane Anchor;
-	@FXML
-	private VBox naviPreview;
-	@FXML
-	private VBox navi;
-	@FXML
-	private VBox homeScene;
-	@FXML
-	private VBox recipeScene;
-	@FXML
-	private VBox compostScene;
-	@FXML
-	private VBox foodScene;
-	@FXML
-	private VBox quizScene;
-	@FXML
-	private VBox prizeScene;
+	
 	@FXML
 	private Text text;
-	
-	boolean run = true;
+    @FXML
+    private JFXButton getStartedBtn;
+    
+    boolean run = true;
 
-	@FXML
-	public void showNavigation(MouseEvent event){
-		Timeline timeline = new Timeline();
-		KeyValue naviXValue = new KeyValue(navi.layoutXProperty(), 0);
-		KeyValue naviPreviewOpacity = new KeyValue(naviPreview.opacityProperty(), 0);
-		
-		KeyFrame keyFrame = new KeyFrame(Duration.millis(300), naviXValue, naviPreviewOpacity);
-		
-		timeline.getKeyFrames().addAll(keyFrame);
-		timeline.play();
-	}
-	
-	@FXML
-	public void hideNavigation(MouseEvent event){
-		Timeline timeline = new Timeline();
-		KeyValue naviXValue = new KeyValue(navi.layoutXProperty(), -150);
-		KeyValue naviPreviewOpacity = new KeyValue(naviPreview.opacityProperty(), 1);
-		
-		KeyFrame keyFrame = new KeyFrame(Duration.millis(300), naviXValue, naviPreviewOpacity);
-		
-		timeline.getKeyFrames().addAll(keyFrame);
-		timeline.play();
-	}
-	
-	@FXML
-	public void changeScene(MouseEvent event) throws IOException {
-		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-		Parent root = null; //(Parent)FXMLLoader.load(getClass().getResource("/theFridge/view/HomePage.fxml"));
-		
-		
-		if(event.getSource().equals(homeScene)){
-			root = FXMLLoader.load(getClass().getResource("/theFridge/view/HomePage.fxml"));
-		}
-		else if(event.getSource().equals(recipeScene)){
-			root = FXMLLoader.load(getClass().getResource("/theFridge/view/RecipePage.fxml"));
-		}
-		else if(event.getSource().equals(compostScene)){
-			root = FXMLLoader.load(getClass().getResource("/theFridge/view/Page1.fxml"));
-		}
-		else if(event.getSource().equals(foodScene)){
-			root = FXMLLoader.load(getClass().getResource("/theFridge/view/ShoppingListPage.fxml"));
-		}
-		else if(event.getSource().equals(quizScene)){
-			root = FXMLLoader.load(getClass().getResource("/theFridge/view/QuizPage.fxml"));
-		}
-		else if(event.getSource().equals(prizeScene)){
-			root = FXMLLoader.load(getClass().getResource("/theFridge/view/RedeemVoucherPage.fxml"));
-		}
-
- 		stage.setScene(new Scene(root));
- 	    stage.show();
-	}
-	
 	@FXML 
 	public void showWord(MouseEvent event) {
 		String word = text.getText();
 		
-		while(run == true){
+		while(run  == true){
 			Timeline timeline = new Timeline(new KeyFrame(Duration.millis(100), new EventHandler<ActionEvent>() {
 			    @Override
 			    public void handle(ActionEvent event) {
@@ -129,5 +59,14 @@ public class HomePageController {
 		}
 		text.setText(displayWord);
 		
+	}
+
+	@FXML 
+	public void changeGetStartedScene(ActionEvent event) throws IOException {
+		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		Parent root = FXMLLoader.load(getClass().getResource("/theFridge/view/GetStartedPage.fxml"));
+		stage.setMaximized(true);
+ 		stage.setScene(new Scene(root));
+ 	    stage.show();
 	}
 }
