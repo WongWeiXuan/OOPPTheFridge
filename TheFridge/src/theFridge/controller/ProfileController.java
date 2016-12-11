c4602c3370014c486f127e0a5f94b938afc00d7a
WongWeiXuan <wong_weixuan@hotmail.com>
2016-12-11 17:49:08 +0800

    Pink profile

--- TheFridge/src/theFridge/controller/ProfileController.java
+++ TheFridge/src/theFridge/controller/ProfileController.java
@@ -12,7 +12,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
