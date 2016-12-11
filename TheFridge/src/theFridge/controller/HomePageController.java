cfb7e5564cd22e39f78c1059fe96e39d72aef93a
WongWeiXuan <wong_weixuan@hotmail.com>
2016-12-11 20:03:28 +0800

    Cant sync

--- TheFridge/src/theFridge/controller/HomePageController.java
+++ TheFridge/src/theFridge/controller/HomePageController.java
@@ -1,24 +1,17 @@
 package theFridge.controller;
 
 import java.io.IOException;
-import java.util.Random;
 
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import com.jfoenix.controls.JFXButton;
+
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
-
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-import com.jfoenix.controls.JFXButton;
 
 public class HomePageController {
 	
@@ -27,39 +20,7 @@ public class HomePageController {
     @FXML
     private JFXButton getStartedBtn;
     
-    boolean run = true;
-
-	@FXML 
-	public void showWord(MouseEvent event) {
-		String word = text.getText();
-		
-		while(run  == true){
-			Timeline timeline = new Timeline(new KeyFrame(Duration.millis(100), new EventHandler<ActionEvent>() {
-			    @Override
-			    public void handle(ActionEvent event) {
-			    	randomiseWord(10, 1000, word);
-			    }
-			}));
-			timeline.setCycleCount(Animation.INDEFINITE);
-			timeline.play();
-			run = false;
-		}
-	}
-	
-	public void randomiseWord(int numOfLetters, int duration, String word){
-		String Characters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-		
-		Random rand = new Random();
-		
-		String displayWord = "";
-		
-		for(int a=0; a < numOfLetters; a++){
-			int randomNum = rand.nextInt(36);
-			displayWord += Characters[randomNum];
-		}
-		text.setText(displayWord);
-		
-	}
+    
 
 	@FXML 
 	public void changeGetStartedScene(ActionEvent event) throws IOException {
