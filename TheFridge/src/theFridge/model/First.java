97696b403250490760d586499b116dcedf097ba6
LimXuanZheng <limxuanzheng888@gmail.com>
2016-12-11 18:27:58 +0800

    step 1 to success

--- TheFridge/src/theFridge/model/First.java
+++ TheFridge/src/theFridge/model/First.java
@@ -5,7 +5,6 @@ import javafx.beans.property.SimpleStringProperty;
 public class First {
 		private final SimpleStringProperty foodCanCompost;
 	 	private final SimpleStringProperty foodCannotCompost;
-		
 		public First(String foodCanCompost, String foodCannotCompost) {
 			super();
 			this.foodCanCompost = new SimpleStringProperty(foodCanCompost);
@@ -15,9 +14,12 @@ public class First {
 		public String getFoodCanCompost() {
 			return foodCanCompost.get();
 		}
-		
+
 		public String getFoodCannotCompost() {
 			return foodCannotCompost.get();
 		}
+
+
+
 		
 }
