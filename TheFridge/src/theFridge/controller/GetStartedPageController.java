18caea086c5a9dca81f92fa596f02343f8c1e210
WongWeiXuan <wong_weixuan@hotmail.com>
2017-01-10 21:22:11 +0800

    Profile Dropdown

--- TheFridge/src/theFridge/controller/GetStartedPageController.java
+++ TheFridge/src/theFridge/controller/GetStartedPageController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
+import javafx.animation.RotateTransition;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +13,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
@@ -60,6 +62,20 @@ public class GetStartedPageController {
 	private ScrollPane LeftoverScroll;
 	@FXML 
 	private ScrollPane LogoutScroll;
+	//For profile dropdown(Profile dropdown)
+	@FXML 
+	private StackPane dropdownMenu;
+	@FXML 
+	private VBox dropdownWord;
+	@FXML 
+	private VBox dropdownBackground;
+	private boolean open = false;
+	@FXML 
+	private VBox ProfileMenu;
+	@FXML 
+	private VBox SettingMenu;
+	@FXML
+	private VBox LogoutMenu;
 	
 	public void initialize(){
 		Image img = new Image("theFridge/picture/Profile Image.jpg");
@@ -68,7 +84,7 @@ public class GetStartedPageController {
 
 	@FXML public void hideExtraInfo(MouseEvent event) {
 		Timeline timeline = new Timeline();
-		KeyValue keyValue1 = new KeyValue(ProfileScroll.vvalueProperty(), 0);
+		KeyValue keyValue1 = new KeyValue(ProfileScroll.vvalueProperty(), 0); //Scroll to the top (0.0)
 		KeyValue keyValue2 = new KeyValue(RecipeScroll.vvalueProperty(), 0);
 		KeyValue keyValue3 = new KeyValue(CompostScroll.vvalueProperty(), 0);
 		KeyValue keyValue4 = new KeyValue(RandomScroll.vvalueProperty(), 0);
@@ -89,7 +105,7 @@ public class GetStartedPageController {
 		KeyValue keyValue1 = null;
 		
 		if(event.getSource().equals(Profile)){
-			keyValue1 = new KeyValue(ProfileScroll.vvalueProperty(), 1.0);
+			keyValue1 = new KeyValue(ProfileScroll.vvalueProperty(), 1.0); //Scroll to the bottom
 		}
 		else if(event.getSource().equals(Recipes)){
 			keyValue1 = new KeyValue(RecipeScroll.vvalueProperty(), 1.0);
@@ -121,10 +137,82 @@ public class GetStartedPageController {
 		timeline.play();
 	}
 	
-	@FXML 
-	public void showProfileDropdown(MouseEvent event) {
-		//TO-DO
-	}
+	//Animation for the Dropdown(Profile Dropdown)
+		@FXML
+		public void showUserDropdown(MouseEvent event) {
+			RotateTransition rt = new RotateTransition(Duration.millis(400), profileCircle);
+			if(open == false){
+				Timeline timeline = new Timeline();
+				rt.setByAngle(-360);
+				rt.play();
+				KeyValue CircleLeft = new KeyValue(profileCircle.translateXProperty(), -46);
+				KeyValue MenuHalf = new KeyValue(dropdownMenu.translateXProperty(), -150); //Move dropdownMenu(dropdownWord & dropdownBackground) translateX to -100
+				KeyFrame keyFrame1 = new KeyFrame(Duration.millis(200), MenuHalf, CircleLeft); //1st KeyFrame with duration of 300ms
+				//Wait
+				KeyFrame WaitingFrame1 = new KeyFrame(
+						Duration.millis(200), 
+						first -> {
+									Timeline timeline3 = new Timeline();
+									KeyValue MenuDown = new KeyValue(dropdownMenu.translateYProperty(), 255); //Move dropdownMenu(dropdownWord & dropdownBackground) translateY to 135
+									KeyValue ShowWord = new KeyValue(dropdownWord.opacityProperty(), 1);
+									KeyFrame keyFrame3 = new KeyFrame(Duration.millis(200), MenuDown, ShowWord); //1st KeyFrame with duration of 300ms
+									
+									timeline3.getKeyFrames().addAll(keyFrame3);
+									timeline3.play();
+						});
+							
+				open = true;
+				timeline.getKeyFrames().addAll(keyFrame1, WaitingFrame1);
+				timeline.play();
+			}
+			else if(open == true){
+				Timeline timeline = new Timeline();
+				rt.setByAngle(360);
+				rt.play();
+				KeyValue MenuDown = new KeyValue(dropdownMenu.translateYProperty(), 0); //Move dropdownMenu(dropdownWord & dropdownBackground) translateY to 135
+				KeyValue HideWord = new KeyValue(dropdownWord.opacityProperty(), 0);
+				KeyFrame keyFrame1 = new KeyFrame(Duration.millis(200), MenuDown, HideWord); //1st KeyFrame with duration of 300ms
+				//Wait
+				KeyFrame WaitingFrame1 = new KeyFrame(
+						Duration.millis(200), 
+						first -> {
+									Timeline timeline3 = new Timeline();
+									KeyValue CircleRight = new KeyValue(profileCircle.translateXProperty(), 0);
+									KeyValue MenuHalf = new KeyValue(dropdownMenu.translateXProperty(), 0); //Move dropdownMenu(dropdownWord & dropdownBackground) translateX to -100
+									KeyFrame keyFrame3 = new KeyFrame(Duration.millis(200), MenuHalf, CircleRight); //1st KeyFrame with duration of 300ms
+									
+									timeline3.getKeyFrames().addAll(keyFrame3);
+									timeline3.play();
+						});
+							
+				open = false;
+				timeline.getKeyFrames().addAll(keyFrame1, WaitingFrame1);
+				timeline.play();
+			}
+			
+			
+			
+		}
+		
+		//Change scene for dropdown(Profile dropdown)
+		@FXML public void menuChangeScene(MouseEvent event) throws IOException {
+			Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+			Parent root = null; //(Parent)FXMLLoader.load(getClass().getResource("/theFridge/view/HomePage.fxml"));
+			
+			
+			if(event.getSource().equals(ProfileMenu)){
+				root = FXMLLoader.load(getClass().getResource("/theFridge/view/Profile.fxml"));
+			}
+			else if(event.getSource().equals(SettingMenu)){
+				root = FXMLLoader.load(getClass().getResource("/theFridge/view/Profile.fxml"));
+			}
+			else if(event.getSource().equals(LogoutMenu)){
+				root = FXMLLoader.load(getClass().getResource("/theFridge/view/LoginPage.fxml"));
+				stage.setMaximized(false);
+			}
+			stage.setScene(new Scene(root));
+	 	    stage.show();
+		}
 	
 	@FXML
 	public void changeScene(MouseEvent event) throws IOException {
