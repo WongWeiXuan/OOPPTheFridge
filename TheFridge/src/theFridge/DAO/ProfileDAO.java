83366fe661ed9edcf94ba89fe5e1f6f1a74e8c0c
LimXuanZheng <limxuanzheng888@gmail.com>
2017-01-14 01:52:08 +0800

    try

--- TheFridge/src/theFridge/DAO/ProfileDAO.java
+++ TheFridge/src/theFridge/DAO/ProfileDAO.java
@@ -2,6 +2,8 @@ package theFridge.DAO;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -10,15 +12,14 @@ import java.util.Scanner;
 import theFridge.model.User;
 
 public class ProfileDAO {
-	private static final String User_File="UserDatas.txt";
-	private File dataFile;
+	private static File dataFile;
 	
 	public ProfileDAO() {
-		Path dPath = FileSystems.getDefault().getPath("resources/data/",User_File);
+		Path dPath = FileSystems.getDefault().getPath("src/theFridge/file/UserDatas.txt");
      	dataFile=new File(dPath.toString());     
 	}
 	
-	public ArrayList<User> getAllFriends() {
+	public static ArrayList<User> getAllUser() {
 		Scanner in;
 		String record=null;
 		String[] fields;
@@ -46,5 +47,59 @@ public class ProfileDAO {
 		}
 		return user;
 	
+	}
+	
+	public User getUser(String username) {
+		ArrayList<User> user = getAllUser();
+		User user1 = null;
+		for (User h : user) {
+			if (h.getUsername().equals(username)){
+				user1 = h;
+				break;
+			}
+		}
+		return user1;
+	}
+	
+	private static void synToFile(ArrayList<User> user) {
+		if (user == null)
+			return;
+		
+		try {
+			FileWriter out = new FileWriter(dataFile);
+			for (User s : user) {
+				out.append(s.toString() + "\n" );
+			}
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean createUser(User user1) {
+		boolean existing = false;
+		ArrayList<User> user = getAllUser();
+		for (User s : user) {
+			if (s.getUsername().equals(user1.getUsername())){
+				existing = true;
+				break;
+			}
+		}
+		if (!existing) {
+			user.add(user1);
+			synToFile(user);
+		}
+		return !existing;
+	}
+	
+	public static void updateUser(User user1) {
+		ArrayList<User> user2 = getAllUser();
+		for (int i = 0; i < user2.size(); i++) {
+			User s = user2.get(i);
+			if (s.getUsername().equals(user1.getUsername())){
+				user2.set(i, user1);
+			}
+		}
+		synToFile(user2);
 	}
 }
