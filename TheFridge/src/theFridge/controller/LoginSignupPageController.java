b635d1aa4e7ccc26396dce9bf6d5e4f97fb456f4
LimXuanZheng <limxuanzheng888@gmail.com>
2017-01-14 01:54:59 +0800

    step by step

--- TheFridge/src/theFridge/controller/LoginSignupPageController.java
+++ TheFridge/src/theFridge/controller/LoginSignupPageController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 //import javafx.stage.StageStyle;
 //import javafx.application.Platform;
 import java.util.ArrayList;
@@ -39,6 +40,7 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 import theFridge.DAO.SignupDAO;
 import theFridge.model.SignupModel;
+import theFridge.model.User;
 
 public class LoginSignupPageController {
 	@FXML
@@ -131,6 +133,22 @@ public class LoginSignupPageController {
 		else if (!Username.equals("") || !Username.equals(null) && !Password.equals("") || !Password.equals(null)) {
 			for (SignupModel s : personList) {
 				if (Username.equals(s.getUsername()) && Password.equals(s.getPassword())) {
+					User one = new User();
+					one.setUsername(Username);
+					one.setPassword(Password);
+					SignupModel p =signupDAO.getPerson(Username);
+					one.setEmail(p.getEmail());
+					one.createUser();
+					String ConfirmUsername = tFUsername.getText();
+					String f="src/theFridge/file/confirm.txt";
+					try{
+						PrintWriter writer = new PrintWriter(f);
+						writer.print("");
+						writer.print(ConfirmUsername);
+						writer.close();
+					}catch (IOException e){
+						e.printStackTrace();
+					}
 					spinner.setOpacity(1);
 					loginBtn.setOpacity(0);
 					
