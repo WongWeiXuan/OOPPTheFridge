4525ef41c19594173810964448fc3abe3b263a25
Axomized <dr.que9@gmail.com>
2017-01-31 00:00:36 +0800

    Oh no...

--- TheFridge/src/theFridge/model/user.java
+++ TheFridge/src/theFridge/model/user.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
 import theFridge.DAO.ProfileDAO;
 
 public class User {
