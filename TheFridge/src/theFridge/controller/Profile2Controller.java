5e188f8653c43bb73dccfb5767432df388f02fdc
LimXuanZheng <limxuanzheng888@gmail.com>
2017-02-04 03:13:55 +0800

    more and more

--- TheFridge/src/theFridge/controller/Profile2Controller.java
+++ TheFridge/src/theFridge/controller/Profile2Controller.java
@@ -31,16 +31,19 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
+import javafx.animation.RotateTransition;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.control.PasswordField;
 
 import javafx.scene.input.MouseEvent;
 
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -231,6 +234,22 @@ public class Profile2Controller {
 	private ImageView face;
 	@FXML
 	private JFXButton btnProfile3;
+	@FXML
+	private StackPane dropdownMenu;
+	@FXML
+	private VBox dropdownBackground;
+	@FXML
+	private VBox dropdownWord;
+	@FXML
+	private VBox ProfileMenu;
+	@FXML
+	private VBox SettingMenu;
+	@FXML
+	private VBox LogoutMenu;
+	@FXML
+	private Circle profileCircle;
+	
+	private boolean open = false;
 
 	public void initialize() throws FileNotFoundException{
 		File file=new File("src/theFridge/file/confirm.txt");
@@ -823,5 +842,76 @@ public class Profile2Controller {
 		image7.setOpacity(0);
 	}
 	
+	public void menuChangeScene(MouseEvent event) throws IOException {
+		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		Parent root = null; //(Parent)FXMLLoader.load(getClass().getResource("/theFridge/view/HomePage.fxml"));
+		
+		
+		if(event.getSource().equals(ProfileMenu)){
+			root = FXMLLoader.load(getClass().getResource("/theFridge/view/Profile.fxml"));
+		}
+		else if(event.getSource().equals(SettingMenu)){
+			root = FXMLLoader.load(getClass().getResource("/theFridge/view/Profile2.fxml"));
+		}
+		else if(event.getSource().equals(LogoutMenu)){
+			root = FXMLLoader.load(getClass().getResource("/theFridge/view/LoginSignupPage.fxml"));
+			stage.setMaximized(false);
+		}
+		stage.setScene(new Scene(root));
+ 	    stage.show();
+	}
+	// Event Listener on Circle[#profileCircle].onMouseClicked
+	@FXML
+	public void showUserDropdown(MouseEvent event) {
+		RotateTransition rt = new RotateTransition(Duration.millis(400), profileCircle);
+		if(open == false){
+			Timeline timeline = new Timeline();
+			rt.setByAngle(-360);
+			rt.play();
+			KeyValue CircleLeft = new KeyValue(profileCircle.translateXProperty(), -46);
+			KeyValue MenuHalf = new KeyValue(dropdownMenu.translateXProperty(), -150); //Move dropdownMenu(dropdownWord & dropdownBackground) translateX to -100
+			KeyFrame keyFrame1 = new KeyFrame(Duration.millis(200), MenuHalf, CircleLeft); //1st KeyFrame with duration of 300ms
+			//Wait
+			KeyFrame WaitingFrame1 = new KeyFrame(
+					Duration.millis(200), 
+					first -> {
+								Timeline timeline3 = new Timeline();
+								KeyValue MenuDown = new KeyValue(dropdownMenu.translateYProperty(), 255); //Move dropdownMenu(dropdownWord & dropdownBackground) translateY to 135
+								KeyValue ShowWord = new KeyValue(dropdownWord.opacityProperty(), 1);
+								KeyFrame keyFrame3 = new KeyFrame(Duration.millis(200), MenuDown, ShowWord); //1st KeyFrame with duration of 300ms
+								
+								timeline3.getKeyFrames().addAll(keyFrame3);
+								timeline3.play();
+					});
+						
+			open = true;
+			timeline.getKeyFrames().addAll(keyFrame1, WaitingFrame1);
+			timeline.play();
+		}
+		else if(open == true){
+			Timeline timeline = new Timeline();
+			rt.setByAngle(360);
+			rt.play();
+			KeyValue MenuDown = new KeyValue(dropdownMenu.translateYProperty(), 0); //Move dropdownMenu(dropdownWord & dropdownBackground) translateY to 135
+			KeyValue HideWord = new KeyValue(dropdownWord.opacityProperty(), 0);
+			KeyFrame keyFrame1 = new KeyFrame(Duration.millis(200), MenuDown, HideWord); //1st KeyFrame with duration of 300ms
+			//Wait
+			KeyFrame WaitingFrame1 = new KeyFrame(
+					Duration.millis(200), 
+					first -> {
+								Timeline timeline3 = new Timeline();
+								KeyValue CircleRight = new KeyValue(profileCircle.translateXProperty(), 0);
+								KeyValue MenuHalf = new KeyValue(dropdownMenu.translateXProperty(), 0); //Move dropdownMenu(dropdownWord & dropdownBackground) translateX to -100
+								KeyFrame keyFrame3 = new KeyFrame(Duration.millis(200), MenuHalf, CircleRight); //1st KeyFrame with duration of 300ms
+								
+								timeline3.getKeyFrames().addAll(keyFrame3);
+								timeline3.play();
+					});
+						
+			open = false;
+			timeline.getKeyFrames().addAll(keyFrame1, WaitingFrame1);
+			timeline.play();
+		}
+	}
 	
 }
--- TheFridge/src/theFridge/controller/ProfileController.java
+++ TheFridge/src/theFridge/controller/ProfileController.java
@@ -9,6 +9,7 @@ import com.jfoenix.controls.JFXButton;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
+import javafx.animation.RotateTransition;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,7 +21,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -108,6 +111,22 @@ public class ProfileController {
     private ImageView face2;
     @FXML
     private JFXButton buton3;
+    @FXML
+	private StackPane dropdownMenu;
+	@FXML
+	private VBox dropdownBackground;
+	@FXML
+	private VBox dropdownWord;
+	@FXML
+	private VBox ProfileMenu;
+	@FXML
+	private VBox SettingMenu;
+	@FXML
+	private VBox LogoutMenu;
+	@FXML
+	private Circle profileCircle;
+	
+	private boolean open = false;
     public void donation(ActionEvent event) throws IOException{
     	Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
 		Parent root = (Parent)FXMLLoader.load(getClass().getResource("/theFridge/view/Profile.fxml"));
@@ -214,7 +233,77 @@ public class ProfileController {
 		stage.setScene(new Scene(root));
  	    stage.show();
 }
-	
+	 public void menuChangeScene(MouseEvent event) throws IOException {
+			Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+			Parent root = null; //(Parent)FXMLLoader.load(getClass().getResource("/theFridge/view/HomePage.fxml"));
+			
+			
+			if(event.getSource().equals(ProfileMenu)){
+				root = FXMLLoader.load(getClass().getResource("/theFridge/view/Profile.fxml"));
+			}
+			else if(event.getSource().equals(SettingMenu)){
+				root = FXMLLoader.load(getClass().getResource("/theFridge/view/Profile2.fxml"));
+			}
+			else if(event.getSource().equals(LogoutMenu)){
+				root = FXMLLoader.load(getClass().getResource("/theFridge/view/LoginSignupPage.fxml"));
+				stage.setMaximized(false);
+			}
+			stage.setScene(new Scene(root));
+	 	    stage.show();
+		}
+	// Event Listener on Circle[#profileCircle].onMouseClicked
+	@FXML
+	public void showUserDropdown(MouseEvent event) {
+		RotateTransition rt = new RotateTransition(Duration.millis(400), profileCircle);
+		if(open == false){
+			Timeline timeline = new Timeline();
+			rt.setByAngle(-360);
+			rt.play();
+			KeyValue CircleLeft = new KeyValue(profileCircle.translateXProperty(), -46);
+			KeyValue MenuHalf = new KeyValue(dropdownMenu.translateXProperty(), -150); //Move dropdownMenu(dropdownWord & dropdownBackground) translateX to -100
+			KeyFrame keyFrame1 = new KeyFrame(Duration.millis(200), MenuHalf, CircleLeft); //1st KeyFrame with duration of 300ms
+			//Wait
+			KeyFrame WaitingFrame1 = new KeyFrame(
+					Duration.millis(200), 
+					first -> {
+								Timeline timeline3 = new Timeline();
+								KeyValue MenuDown = new KeyValue(dropdownMenu.translateYProperty(), 255); //Move dropdownMenu(dropdownWord & dropdownBackground) translateY to 135
+								KeyValue ShowWord = new KeyValue(dropdownWord.opacityProperty(), 1);
+								KeyFrame keyFrame3 = new KeyFrame(Duration.millis(200), MenuDown, ShowWord); //1st KeyFrame with duration of 300ms
+								
+								timeline3.getKeyFrames().addAll(keyFrame3);
+								timeline3.play();
+					});
+						
+			open = true;
+			timeline.getKeyFrames().addAll(keyFrame1, WaitingFrame1);
+			timeline.play();
+		}
+		else if(open == true){
+			Timeline timeline = new Timeline();
+			rt.setByAngle(360);
+			rt.play();
+			KeyValue MenuDown = new KeyValue(dropdownMenu.translateYProperty(), 0); //Move dropdownMenu(dropdownWord & dropdownBackground) translateY to 135
+			KeyValue HideWord = new KeyValue(dropdownWord.opacityProperty(), 0);
+			KeyFrame keyFrame1 = new KeyFrame(Duration.millis(200), MenuDown, HideWord); //1st KeyFrame with duration of 300ms
+			//Wait
+			KeyFrame WaitingFrame1 = new KeyFrame(
+					Duration.millis(200), 
+					first -> {
+								Timeline timeline3 = new Timeline();
+								KeyValue CircleRight = new KeyValue(profileCircle.translateXProperty(), 0);
+								KeyValue MenuHalf = new KeyValue(dropdownMenu.translateXProperty(), 0); //Move dropdownMenu(dropdownWord & dropdownBackground) translateX to -100
+								KeyFrame keyFrame3 = new KeyFrame(Duration.millis(200), MenuHalf, CircleRight); //1st KeyFrame with duration of 300ms
+								
+								timeline3.getKeyFrames().addAll(keyFrame3);
+								timeline3.play();
+					});
+						
+			open = false;
+			timeline.getKeyFrames().addAll(keyFrame1, WaitingFrame1);
+			timeline.play();
+		}
+	}
 		
 
 }
